7efa84bfef1eef1c599543ac400438b58d58957d
Alejandro Paredes Alva <aparedes@aparedes.net>
2014-03-19 14:00:41 +0100

    Adds test for method usingThis:addThis:
    Tests what you can and can not add at the end of the original string

--- Demo/GIDACalculateStringTests/GIDACalculateStringTests.m
+++ Demo/GIDACalculateStringTests/GIDACalculateStringTests.m
@@ -3,15 +3,26 @@
 //  GIDACalculateStringTests
 //
 //  Created by Alejandro Paredes Alva on 19/03/14.
-//  Copyright (c) 2014 Alejandro Paredes Alva. All rights reserved.
+//  Copyright (c) 2014 GIDAIbero. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
+#import "GIDACalculateString.h"
 
 @interface GIDACalculateStringTests : XCTestCase
 
 @end
 
+
+/* Methods to check
+ +(BOOL)usingThis:(NSString *)string addThis:(NSString *)newString;
+ +(BOOL)usingThis:(NSString *)string addThis:(NSString *)newString here:(NSRange)range;
+ 
+ +(NSString *)stringFrom:(NSString *)string withThis:(NSString *)newString;
+ +(NSString *)stringFrom:(NSString *)string withThis:(NSString *)newString here:(NSRange)range;
+ 
+ +(NSNumber *)solveString:(NSString *)string;
+*/
 @implementation GIDACalculateStringTests
 
 - (void)setUp
@@ -26,9 +37,53 @@
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testUsingThisAddThis {
+    NSString *usingThis;
+    NSArray  *addThisArray;
+    NSString *addThis;
+    
+    usingThis    = @"";
+    addThisArray =  @[@"+",@"*",@"/",@")"];
+    for (addThis in addThisArray) {
+        XCTAssertFalse([GIDACalculateString usingThis:usingThis addThis:addThis], @"GIDACalculateString usingThis:addThis:\tFailed to return false using:%@ and adding:%@",usingThis, addThis);
+    }
+    
+    usingThis    = @"";
+    addThisArray = @[@"-",@"(",@"0",@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",];
+    for (NSString *addThis in addThisArray) {
+        XCTAssertTrue([GIDACalculateString usingThis:usingThis addThis:addThis], @"GIDACalculateString usingThis:addThis:\tFailed to return true using:%@ and adding:%@",usingThis, addThis);
+    }
+   
+    usingThis    = @".";
+    addThisArray = @[@".",@"+",@"-",@"*",@"/",@"(",@")"];
+    for (addThis in addThisArray) {
+        XCTAssertFalse([GIDACalculateString usingThis:usingThis addThis:addThis], @"GIDACalculateString usingThis:addThis: Failed to return false using:%@ and adding:%@", usingThis, addThis);
+    }
+    usingThis    = @"(";
+    addThisArray = @[@"+",@"*",@"/",@")"];
+    for (addThis in addThisArray) {
+        XCTAssertFalse([GIDACalculateString usingThis:usingThis addThis:addThis], @"GIDACalculateString usingThis:addThis: Failed to return false using:%@ and adding:%@", usingThis, addThis);
+    }
+    
+    usingThis    = @"3";
+    addThisArray = @[@")"];
+    for (addThis in addThisArray) {
+        XCTAssertFalse([GIDACalculateString usingThis:usingThis addThis:addThis], @"GIDACalculateString usingThis:addThis: Failed to return false using:%@ and adding:%@", usingThis, addThis);
+    }
+    
+    usingThis    = @"3.5";
+    addThisArray = @[@"."];
+    for (addThis in addThisArray) {
+        XCTAssertFalse([GIDACalculateString usingThis:usingThis addThis:addThis], @"GIDACalculateString usingThis:addThis: Failed to return false using:%@ and adding:%@", usingThis, addThis);
+    }
+    
+    usingThis    = @"-";
+    addThisArray = @[@"+",@"-",@"*",@"/",@")"];
+    for (addThis in addThisArray) {
+        XCTAssertFalse([GIDACalculateString usingThis:usingThis addThis:addThis], @"GIDACalculateString usingThis:addThis: Failed to return false using:%@ and adding:%@", usingThis, addThis);
+    }
 }
+
+
 
 @end
